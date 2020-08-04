56062ed80fe5ba4cd618399ec3372e2fe3430b51
saxenabhishek <as7122000@gmail.com>
2020-08-04 17:21:07 +0530

    Frame

--- Controler.py
+++ Controler.py
@@ -1,13 +1,62 @@
 import json
 import pandas_datareader as web
 import pandas as pd
+import datetime
+import pymongo
 
-info = json.loads(open("info.json","r").read())
+
+
+#info = json.loads(open("info.json","r").read())
 
 def updatedata(stocks):
     for i in stocks:
         df=web.DataReader(i,data_source='yahoo')
-        df.to_csv("data_"+i+".csv")
+        df.to_csv("stockdata/data_"+i+".csv")
 
-updatedata(info["Stocks"])        
+#updatedata(info["Stocks"])
+
+
+user = "Abhishek"
+password = "681dmxUsOW4DZWlr"
+
+
+client = pymongo.MongoClient(f"mongodb+srv://{user}:{password}@clust01.7nxse.mongodb.net")
+
+db = client.predictions
+
+today = datetime.datetime.today()
+start = datetime.datetime(today.year,today.month,3,0,0)
+end = datetime.datetime(today.year,today.month,3,23,59)
+print(start,end,datetime.datetime.now())
+#db.acess.insert_one({"date":datetime.datetime.now()})
+
+print(db.acess.find_one({'date':{'$lt': end, '$gte': start}}))
+
+print("done now")
+
+post = {
+            "date":datetime.datetime.now()
+            "stockname" : {
+                "7":[],
+                "14":[],
+                "21":[],
+            }
+        }
+
+
+
+if __name__ == "__main__":
+    if EntryExists() == False:
+        A = AllModelsDontExist()
+        if A is not None:
+            MakeTrainSave(A)
+        dat = MakePreds()
+        post = MakeReq(dat)
+        db.data.insert_one{post}
+        
+        
+         
+
+
+
     
--- StockModel.py
+++ StockModel.py
@@ -1,20 +1,31 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+#import tensorflow as tf
 
-import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from tensorflow import convert_to_tensor
+
+#import matplotlib.pyplot as plt
 
 from tensorflow.keras import layers
 from sklearn.preprocessing import MinMaxScaler
 
-tf.random.set_seed(7)
-np.random.seed(7)
+#tf.random.set_seed(7)
+#np.random.seed(7)
 
 class Model_related_things():
     def __init__(self,Outputdays = 7, inputdays = 30):
 
         self.Outputdays = Outputdays
         self.inputdays = inputdays
+        self.scaler = MinMaxScaler(feature_range=(0,0.75))
+
+    def loadmodel(self,path):
+        
+        self.model = load_model(path)
+
+
+    def newmodel(self):
 
         tf.keras.backend.clear_session()
         self.model = tf.keras.Sequential()
@@ -23,8 +34,9 @@ class Model_related_things():
         for j in [128,128]:
             self.model.add(layers.LSTM(j, return_sequences=True))
         self.model.add(layers.LSTM(128))
-        for i in [64,64,Outputdays]:
+        for i in [64,64]:
             self.model.add(layers.Dense(i, activation="selu"))
+        self.model.add(layers.Dense(Outputdays))
 
     def plsgivedataset(self, series):
         window_size = self.inputdays
@@ -43,11 +55,8 @@ class Model_related_things():
         return ds
     
     def trainModel(self, traindata):
-
-        scaler = MinMaxScaler(feature_range=(0,0.75))
-
         ser = traindata.reshape(-1,1)
-        series = scaler.fit_transform(ser)
+        series = self.scaler.fit_transform(ser)
         
         ds = self.plsgivedataset(series)
 
@@ -55,20 +64,33 @@ class Model_related_things():
         l = tf.keras.losses.LogCosh()
         self.model.compile(optimizer = "SGD", loss = "mse" , metrics=[l])
         
-        h = self.model.fit(ds,epochs=20,verbose=1)
+        h = self.model.fit(ds,epochs=20,verbose=2)
         hist = h.history
 
         return hist
 
+    def ServePred(self,series):
+        inpred = series[-self.inputdays:]
+        inpred = inpred.reshape(-1,1)
+        inpred = self.scaler.fit_transform(inpred)
+        inpred = inpred.reshape(1,-1,1)
+        inpred = convert_to_tensor(inpred)
 
+        print(self.model.predict(inpred))
+
+
+print("importin")
 Apple_data = pd.read_csv("data_googl.csv")
 
 window = 30
 predday = 7
 
-print(len(Apple_data))
+#print(len(Apple_data))
 
 newmodel = Model_related_things(predday,window)
-plt.plot(newmodel.trainModel(Apple_data.Close.values)["loss"])
-plt.show()
+newmodel.loadmodel("Experiments NB/Model1_pred_7days.h5")
+newmodel.ServePred(Apple_data.Close.values)
+
+#plt.plot(newmodel.trainModel(Apple_data.Close.values)["loss"])
+#plt.show()
 
